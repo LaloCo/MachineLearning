bb2d36fc3f460ab6a0a3abbe77428f21bfc7c359
Eduardo Rosas <eduardo@abmx.onmicrosoft.com>
2017-11-23 18:08:29 -0600

    Changed word data from dos to unix (with the dos2unix file), modified the files inside the tools folder to work with Python 3, and solved the mini project

--- ud120/naive_bayes/nb_author_id.py
+++ ud120/naive_bayes/nb_author_id.py
@@ -11,10 +11,12 @@
 """
     
 import sys
+import os
 from time import time
-sys.path.append("../tools/")
+#sys.path.append("../tools/")
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(CURRENT_DIR) + "/tools/")
 from email_preprocess import preprocess
-
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -26,8 +28,14 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
 
+classifier = GaussianNB()
+classifier.fit(features_train, labels_train)
+GaussianNB(priors=None)
+predictions = classifier.predict(features_test)
+
+accuracy = classifier.score(features_test, labels_test)
+print(accuracy)
 
 #########################################################
-
-
