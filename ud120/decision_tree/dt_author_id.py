d9c117a4c263609055b740d40d5744b7e9a85590
Eduardo Rosas <eduardo@abmx.onmicrosoft.com>
2017-11-30 12:39:58 -0600

    Decision tree algorithm has been coded and tested with 10% and 1% of the features

--- ud120/decision_tree/dt_author_id.py
+++ ud120/decision_tree/dt_author_id.py
@@ -10,6 +10,7 @@
     
 import sys, os
 from time import time
+from sklearn import tree
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(CURRENT_DIR) + "/tools/")
 from email_preprocess import preprocess
@@ -23,6 +24,31 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+classifier = tree.DecisionTreeClassifier(min_samples_split=40)
 
+print("no. of features: " + str(len(features_train[0]))) # Number of rows is data points, number of columns is number of features
+
+t0 = time()
+classifier.fit(features_train, labels_train)
+print("Training time: ", round(time() - t0, 3), "s")
+
+t0 = time()
+predictions = classifier.predict(features_test)
+print("Prediction time: ", round(time() - t0, 3), "s")
+
+accuracy = classifier.score(features_test, labels_test)
+print(accuracy)
 
 #########################################################
+
+# Times & Accuracy w/ min_samples_split = 40
+## Number of features: 3785
+## Training time: 126.773s
+## Prediction time: 0.082s
+## Accuracy: 0.977815699659
+
+# Times & Accuracy w/ min_samples_split = 40 & w/ percentile set to only 1 from email_preprocess (1% of the features is used)
+## Number of features: 379
+## Training time: 7.442s
+## Prediction time: 0.002s
+## Accuracy: 0.966439135381
