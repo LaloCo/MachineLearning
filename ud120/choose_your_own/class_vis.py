105a2e0e41c2874a2573f3fbf43f3ce6f61e692b
Eduardo Rosas <lalorosas@outlook.com>
2017-12-01 12:32:26 -0600

    Solved the "CYOA" challenge with kNN Classifier, got ~94% accuracy with very low training times

--- ud120/choose_your_own/class_vis.py
+++ ud120/choose_your_own/class_vis.py
@@ -19,7 +19,8 @@ def prettyPicture(clf, X_test, y_test):
     plt.xlim(xx.min(), xx.max())
     plt.ylim(yy.min(), yy.max())
 
-    plt.pcolormesh(xx, yy, Z, cmap=pl.cm.seismic)
+    #? plt.pcolormesh(xx, yy, Z, cmap=pl.cm.seismic())
+    plt.pcolormesh(xx, yy, Z, cmap='RdBu')
 
     # Plot also the test points
     grade_sig = [X_test[ii][0] for ii in range(0, len(X_test)) if y_test[ii]==0]
--- ud120/choose_your_own/your_algorithm.py
+++ ud120/choose_your_own/your_algorithm.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 
 import matplotlib.pyplot as plt
+from time import time
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+from sklearn.neighbors import KNeighborsClassifier
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -31,14 +33,66 @@ plt.show()
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+clf = KNeighborsClassifier(n_neighbors=1, weights='distance')
+t0 = time()
+clf.fit(features_train, labels_train)
+print("Training time: ", round(time() - t0, 3), "s")
 
+t0 = time()
+predictions = clf.predict(features_test)
+print("Prediction time: ", round(time() - t0, 3), "s")
 
-
-
-
-
+accuracy = clf.score(features_test, labels_test)
+print(accuracy)
 
 try:
     prettyPicture(clf, features_test, labels_test)
 except NameError:
     pass
+
+#########################################################
+
+# Times & Accuracy with n_neighbors = 3 & weight = 'uniform'
+## Training time: 0.002s
+## Prediction time: 0.002s
+## Accuracy: 0.936
+
+# Times & Accuracy with n_neighbors = 5 & weight = 'uniform'
+## Training time: 0.002s
+## Prediction time: 0.002s
+## Accuracy: 0.92
+
+# Times & Accuracy with n_neighbors = 1 & weight = 'uniform'
+## Training time: 0.002s
+## Prediction time: 0.007s
+## Accuracy: 0.94
+
+# Times & Accuracy with n_neighbors = 1 & weight = 'distance'
+## Training time: 0.002s
+## Prediction time: 0.002s
+## Accuracy: 0.94
+
+# Times & Accuracy with n_neighbors = 3 & weight = 'distance'
+## Training time: 0.003s
+## Prediction time: 0.003s
+## Accuracy: 0.936
+
+# Times & Accuracy with n_neighbors = 1 & weight = 'distance' & algorithm = 'auto'
+## Training time: 0.002s
+## Prediction time: 0.003s
+## Accuracy: 0.94
+
+# Times & Accuracy with n_neighbors = 1 & weight = 'distance' & algorithm = 'ball_tree'
+## Training time: 0.005s
+## Prediction time: 0.003s
+## Accuracy: 0.94
+
+# Times & Accuracy with n_neighbors = 1 & weight = 'distance' & algorithm = 'kd_tree'
+## Training time: 0.004s
+## Prediction time: 0.002s
+## Accuracy: 0.94
+
+# Times & Accuracy with n_neighbors = 1 & weight = 'distance' & algorithm = 'brute'
+## Training time: 0.001s
+## Prediction time: 0.019s
+## Accuracy: 0.94
