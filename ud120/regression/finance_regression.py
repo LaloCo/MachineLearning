c1add33977bba6045c18625a465461a0c6b6b353
Eduardo Rosas <lalorosas@outlook.com>
2017-12-08 16:35:33 -0600

    Regression trained with test data (without outliers)

--- ud120/regression/finance_regression.py
+++ ud120/regression/finance_regression.py
@@ -16,6 +16,7 @@
 import sys
 import os
 import pickle
+from sklearn.linear_model import LinearRegression
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(CURRENT_DIR) + "/tools/")
 from feature_format import featureFormat, targetFeatureSplit
@@ -39,13 +40,16 @@ test_color = "r"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
-
-
-
-
-
-
-
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
+slope = reg.coef_
+intercept = reg.intercept_
+# score = reg.score(feature_train, target_train) # Quiz where same training data is used for testing
+score = reg.score(feature_test, target_test) 
+print("Trained with train data:")
+print(slope)
+print(intercept)
+print(score)
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
@@ -64,6 +68,15 @@ plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
 ### draw the regression line, once it's coded
 try:
     plt.plot( feature_test, reg.predict(feature_test) )
+    reg.fit(feature_test, target_test)
+    plt.plot(feature_train, reg.predict(feature_train), color="b") 
+    slope = reg.coef_
+    intercept = reg.intercept_
+    score = reg.score(feature_test, target_test) 
+    print("Trained with test data:")
+    print(slope)
+    print(intercept)
+    print(score)
 except NameError:
     pass
 plt.xlabel(features_list[1])
