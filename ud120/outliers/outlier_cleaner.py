ccacfc737e26a31debaa55c3eda7de8fa3dfb7cc
Eduardo Rosas <lalorosas@outlook.com>
2017-12-20 09:39:48 -0600

    Outlier cleaner now cleaning the 10% with bigger errors

--- ud120/outliers/outlier_cleaner.py
+++ ud120/outliers/outlier_cleaner.py
@@ -12,9 +12,29 @@ def outlierCleaner(predictions, ages, net_worths):
     """
     
     cleaned_data = []
+    errors = []
+    cleaned_errors = []
 
     ### your code goes here
+    count = 0
+    for p in predictions:
+        errors.append((net_worths[count] - p) * (net_worths[count] - p))
+        count = count + 1
+    
+    sorted_errors = sorted(errors)
+    outlier_definer = sorted_errors[80]
 
+    cleaned_net_worths = net_worths[errors < outlier_definer]
+    cleaned_ages = ages[errors < outlier_definer]
+    cleaned_predictions = predictions[errors < outlier_definer]
+
+    print(len(cleaned_net_worths))
+
+    count = 0
+    for p in cleaned_predictions:
+        cleaned_errors.append((cleaned_net_worths[count] - p) * (cleaned_net_worths[count] - p))
+        count = count + 1
+
+    cleaned_data = tuple(zip(cleaned_ages, cleaned_net_worths, cleaned_errors))
     
     return cleaned_data
-
