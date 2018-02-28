82258270f252ba08ca17a7cdb605240b7c51c383
Eduardo Rosas <lalorosas@outlook.com>
2018-02-28 08:39:41 -0600

    Feature scaling implemented with MinMacScaler from sklearn.preprocessing

--- ud120/k_means/k_means_cluster.py
+++ ud120/k_means/k_means_cluster.py
@@ -9,6 +9,7 @@ import numpy
 import matplotlib.pyplot as plt
 import sys, os
 from sklearn.cluster import KMeans
+from sklearn import preprocessing
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(CURRENT_DIR) + "/tools/")
 from feature_format import featureFormat, targetFeatureSplit
@@ -49,23 +50,39 @@ feature_1 = "salary"
 feature_2 = "exercised_stock_options"
 feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2, feature_3]
+features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
 
-maxValue = data_dict[ max(data_dict, key = lambda x : -float("inf") if data_dict[x][feature_2] == "NaN" else data_dict[x][feature_2])][feature_2]
+maxValue = data_dict[ max(data_dict, key = lambda x : -float("inf") if data_dict[x][feature_1] == "NaN" else data_dict[x][feature_1])][feature_1]
 
-minValue = data_dict[ min(data_dict, key = lambda x : float("inf") if data_dict[x][feature_2] == "NaN" else data_dict[x][feature_2])][feature_2]
+minValue = data_dict[ min(data_dict, key = lambda x : float("inf") if data_dict[x][feature_1] == "NaN" else data_dict[x][feature_1])][feature_1]
 
-print(maxValue, minValue)
+maxValue2 = data_dict[ max(data_dict, key = lambda x : -float("inf") if data_dict[x][feature_2] == "NaN" else data_dict[x][feature_2])][feature_2]
 
+minValue2 = data_dict[ min(data_dict, key = lambda x : float("inf") if data_dict[x][feature_2] == "NaN" else data_dict[x][feature_2])][feature_2]
+
+# print(maxValue, minValue)
+# print(maxValue2, minValue2)
+
+
+### FEATURE SCALING
+min_max_scaler = preprocessing.MinMaxScaler()
+
+x_train = featureFormat(data_dict, [feature_1, feature_2])
+x_train_minmax = min_max_scaler.fit_transform(x_train)
+# print(x_train_minmax)
+
+x_test = numpy.array([[200000., 1000000.]])
+x_test_minmax = min_max_scaler.transform(x_test)
+print(x_test_minmax)
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2, _ in finance_features:
+for f1, f2 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
@@ -79,6 +96,6 @@ pred = kmeans.predict(finance_features)
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters_3f.pdf", f1_name=feature_1, f2_name=feature_2)
+    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print("no predictions object named pred found, no clusters to plot")
